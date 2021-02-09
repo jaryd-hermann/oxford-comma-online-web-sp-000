c1fbb9a410c321b67fc336310e74beeff6a954d3
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-09 23:56:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-array.split
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    array.insert(-2, "and")
+    array[0..-1].join(", ")
+  else
+    array.join
 end
